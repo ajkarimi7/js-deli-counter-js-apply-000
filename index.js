4a6b68892fc64522cd74455f7523e53785592941
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-09 16:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ function takeANumber(katzDeli, name) {
 
 function nowServing(katzDeliLine) {
   
-  let i = 0
-  
-  while (i < katzDeliLine.length) {
-    i++;
-  }
-  
   if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!";
 } 
